26f907b7fdffd3886c8f9b32506916d068e276ae
Enrico <eddmler@gmail.com>
2017-03-01 14:41:33 +0100

    Add http error views

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel-heading">Fehler 403: Verboten</div>
+
+<div class="panel-body">
+
+<div class="alert alert-danger">
+  <ul>
+    <li>Du hast keine Berechtigung diese Aktion durchzuführen.</li>
+  </ul>
+</div>
+</div>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel-heading">Fehler 404: Nicht gefunden</div>
+
+<div class="panel-body">
+
+<div class="alert alert-danger">
+  <ul>
+    <li>Die von dir gesuchte Seite wurde nicht gefunden.</li>
+  </ul>
+</div>
+</div>
+@endsection
