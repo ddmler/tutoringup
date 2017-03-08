78c699feab31850a360466251b879637d2a45ba6
Enrico <eddmler@gmail.com>
2017-03-08 14:02:28 +0100

    Add more feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
 
 class ExampleTest extends TestCase
 {
@@ -27,7 +28,7 @@ class ExampleTest extends TestCase
         $response = $this->get('/inserate');
 
         $response->assertStatus(200);
-        $response->assertSee("Inserat Übersicht");
+        $response->assertSee("Biete Tutor:"); // second result
     }
 
     public function testInserateFilter()
@@ -36,6 +37,24 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertSee("Aktueller Filter:");
+
+        $response2 = $this->get('/inserate/filter/suche/student');
+
+        $response2->assertStatus(200);
+        $response2->assertSee("Suche Tutor:"); // first result
+
+        $response3 = $this->get('/inserate/filter/suche/student/1');
+
+        $response3->assertStatus(200);
+        $response3->assertSee("Suche Tutor:"); // first result
+    }
+
+    public function testInserateView()
+    {
+        $response = $this->get('/inserate/1');
+
+        $response->assertStatus(200);
+        $response->assertSee("von: test"); // creator
     }
 
     public function testAltklausuren()
@@ -43,7 +62,7 @@ class ExampleTest extends TestCase
         $response = $this->get('/altklausuren');
 
         $response->assertStatus(200);
-        $response->assertSee("Altklausuren Übersicht");
+        $response->assertSee("(PNG)"); // from a result
     }
 
     public function testAltklausurenFilter()
@@ -51,6 +70,66 @@ class ExampleTest extends TestCase
         $response = $this->get('/altklausuren/1');
 
         $response->assertStatus(200);
-        $response->assertSee("Aktueller Filter:");
+        $response->assertSee("(JPG)"); // from a result
+    }
+
+    public function testCreateInserat()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/inserate/create');
+
+        $response->assertStatus(200);
+        $response->assertSee("Titel"); // a label for a form field
+    }
+
+    public function testOwnInserat()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/inserate/own');
+
+        $response->assertStatus(200);
+        $response->assertSee("Suche Tutor"); // first result
+    }
+
+    public function testEditInserat()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/inserate/1/edit');
+
+        $response->assertStatus(200);
+        $response->assertSee("Titel"); // a label for a form field
+    }
+
+    public function testEditInseratFail()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/inserate/2/edit'); // try to edit a post belonging to another user
+
+        $response->assertStatus(403);
+        $response->assertSee("Fehler"); // error
+    }    
+
+    public function testCreateAltklausur()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/altklausuren/create');
+
+        $response->assertStatus(200);
+        $response->assertSee("Titel"); // a label for a form field
+    }
+
+    public function testOwnAltklausur()
+    {
+        $user = User::find(1); // user test
+
+        $response = $this->actingAs($user)->get('/altklausuren/own');
+
+        $response->assertStatus(200);
+        $response->assertSee("(JPG)"); // first result
     }
 }
